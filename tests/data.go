1aa3f592b16d78853fac15cc0c1cc8417e5bd753
Rafael Moreira <rafadev7@gmail.com>
2016-04-26 22:20:37 -0300

    Adding more products for test

--- tests/data.go
+++ tests/data.go
@@ -22,7 +22,9 @@ var (
 	// Initializes in InitTestDB
 	TokenTest = &models.Token{}
 	// The first test product
-	ProductTest = &models.Product{Gtin: "7894900700046", Description: "REFRIGERANTE COCA COLA LATA ZERO 350ML", Thumbnail: "https://s3.amazonaws.com/bluesoft-cosmos/products/7894900700220/zhksxcua", Price: 4.53, PriceMax: 6.30, PriceMin: 3.90}
+	ProductTest1 = &models.Product{Gtin: "7894900700046", Description: "REFRIGERANTE COCA COLA LATA ZERO 350ML", Thumbnail: "https://s3.amazonaws.com/bluesoft-cosmos/products/7894900700220/zhksxcua", Price: 4.53, PriceMax: 6.30, PriceMin: 3.90}
+	ProductTest2 = &models.Product{Gtin: "0789840233945", Description: "FANDANGOS SALGADINHO DE MILHO SABOR PRESUNTO (HAM FLAVOR SNACK) BAG 63G -BRAZIL", Thumbnail: "https://s3.amazonaws.com/bluesoft-cosmos/products/789840233945", Price: 21.10, PriceMax: 22.10, PriceMin: 20.10}
+	ProductTest3 = &models.Product{Gtin: "7892840211240", Description: "SALGADINHO DORITOS QUEIJO NACHO 110G", Thumbnail: "https://s3.amazonaws.com/bluesoft-cosmos/products/doritos-queijo-nacho-elma-chips-110-g_600x600-PU6a203_1.jpg", Price: 4.53, PriceMax: 6.30, PriceMin: 3.90}
 )
 
 // Creating temporary test schemas
@@ -57,7 +59,17 @@ func InsertTestEntityies() error {
 	}
 
 	// Adding ProductTest
-	err = ProductTest.Insert(nil)
+	err = ProductTest1.Insert(nil)
+	if err != nil {
+		return err
+	}
+	// Adding ProductTest
+	err = ProductTest2.Insert(nil)
+	if err != nil {
+		return err
+	}
+	// Adding ProductTest
+	err = ProductTest3.Insert(nil)
 	if err != nil {
 		return err
 	}
